5b4758df5e966414d69a1e19a6ffd78ebc132509
isiRichard <isi.12ichard@gmail.com>
2016-05-29 22:31:25 -0500

    initiating brain

---
+++ src/controllerClinicaDental/Log_in.java
@@ -0,0 +1,14 @@
+package controllerClinicaDental;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.*;
+
+
+@SuppressWarnings("serial")
+public class Log_in extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+	}
+}
